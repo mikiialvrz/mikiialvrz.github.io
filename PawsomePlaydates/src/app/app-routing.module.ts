b10097c646d3f034713a77324d10ed7da82e11cd
EvanDyson <evandyson@ufl.edu>
2023-09-09 14:02:48 -0400

    updates to format. changed to one page

--- PawsomePlaydates/src/app/app-routing.module.ts
+++ PawsomePlaydates/src/app/app-routing.module.ts
@@ -1,20 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ScheduleComponent } from './schedule/schedule.component';
-import { PriceSheetComponent } from './price-sheet/price-sheet.component';
-import { AboutMeComponent } from './about-me/about-me.component';
 import { HomeComponent } from './home/home.component';
 
 
 const routes: Routes = [
-    { path: '', component: HomeComponent },
-    { path: 'schedule', component: ScheduleComponent },
-    { path: 'prices', component: PriceSheetComponent },
-    { path: 'about', component: AboutMeComponent },
+    { path: '', component: HomeComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
